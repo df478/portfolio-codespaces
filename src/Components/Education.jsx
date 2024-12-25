22ab89a8f306001d1225970d17e24d56cf52d450
Eleazar <eledavid88@gmail.com>
2024-12-25 02:29:34 +0000

    Extra with copilot

---
+++ src/Components/Education.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section id="education" className="min-height">
+      <h1>Education</h1>
+      <div>
+        <h2>Universidad Mayor De San Andres</h2>
+        <p>Bachelor in Computer Science</p>
+        <p>2021-2025</p>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
